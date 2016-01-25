c7d1425cdd9bfca72610c3019e6af8f8f07525ce
Tony Artimisi <Tony Artimisi>
2016-01-25 17:18:04 -0500

    found a typo on page 108 of the text... he left out and 'event.' command and I got error after error.

--- chapter4/Example 2/app.js
+++ chapter4/Example 2/app.js
@@ -13,6 +13,18 @@ var main = function() {
 
     });
 
+    $(".comment-input input").on("keypress", function(event) {
+    	if (event.keyCode === 13) {
+    		if ($(".comment-input input").val() !== "") {
+            var $new_comment = $("<p>").text($(".comment-input input").val());
+            $(".comments").append($new_comment);
+            $(".comment-input input").val("");
+        }
+    		
+    	}
+
+    });
+
 };
 
 $(document).ready(main);
