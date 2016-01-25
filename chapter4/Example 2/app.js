a6011306efeeab44c99ad9a3ae9d66eda25f3c45
Tony Artimisi <Tony Artimisi>
2016-01-25 15:39:35 -0500

    This is the 'this is a new comment' js

--- chapter4/Example 2/app.js
+++ chapter4/Example 2/app.js
@@ -1,9 +1,9 @@
-var main = function () {
-	"use strict";
+var main = function() {
+    "use strict";
 
-	$(".comment-input button").on("click", function (event) {
-		console.log("Hello World!");
-	});
+    $(".comment-input button").on("click", function(event) {
+        $(".comments").append("<p>this is a new comment</p>");
+    });
 };
 
 $(document).ready(main);
