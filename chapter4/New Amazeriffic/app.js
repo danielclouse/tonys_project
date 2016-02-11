a064071e64a2894702845f610854a0c5544a57a2
Tony Artimisi <Tony Artimisi>
2016-02-10 21:16:35 -0500

    the array

--- chapter4/New Amazeriffic/app.js
+++ chapter4/New Amazeriffic/app.js
@@ -1,31 +1,14 @@
-var main = function () {
+var main = function() {
     "use strict";
-
-    var makeTabsActive = function (tabNumber) {
-        //construct the selector from the tabNumber
-        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-        $(".tabs span").removeClass("active");
-        $(tabSelector).addClass("active");
-    };
-
-    $(".tabs a:nth-child(1)").on("click", function () {
-        makeTabsActive(1);
-        return false;
+    $(".tabs a span").toArray().forEach(function(element) {
+        // create a click handler for this element
+        $(element).on("click", function() {
+            $(".tabs a span").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
+            return false;
+        });
     });
-
-    $(".tabs a:nth-child(2)").on("click", function () {
-        makeTabsActive(2);
-        return false;
-    });
-
-    $(".tabs a:nth-child(3)").on("click", function () {
-        makeTabsActive(3);
-        return false;
-    });
-
-    
-
 };
 
 $(document).ready(main);
-
